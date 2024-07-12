2bb4da2224bc8835e6508c337aad5a1bf545e0f4
duechayapolgmit <duechayapolsbs@gmail.com>
2024-07-12 16:49:10 +0100

    dings and countdown

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const res = await fetch("./items.json")
 const itemsJSON = await res.json()
 
+const TIMER = 17
+
 // Initialise with Event Format
 document.getElementById("title").innerText = await itemsJSON[0].title;
 document.getElementById("picture").src = await itemsJSON[0].picture;
 
-var screenRotate = setInterval(rotate, 15100)
+var screenRotate = setInterval(rotate, 15080)
 
+let prev = 0
 async function rotate() {
     let randSlide = Math.floor(Math.random() * (itemsJSON.length-1)) + 1
+    if (prev == randSlide) randSlide = randSlide + 1;
+
+    prev = randSlide
 
     document.getElementById("title").innerText = await itemsJSON[randSlide].title;
     document.getElementById("picture").src = await itemsJSON[randSlide].picture;
+}
+
+var time = TIMER;
+var countdown = setInterval(minusTime, 1000)
+var countdownSound = document.getElementById("sound");
+
+function minusTime() {
+    if (time > 0) time--;
+
+    if (time <= 15 && time > 0) {
+        countdownSound.load()
+        countdownSound.play()
+    }
+
+    document.getElementById("countdown").innerText = new Date(time * 1000).toISOString().substring(15, 19);
 }
