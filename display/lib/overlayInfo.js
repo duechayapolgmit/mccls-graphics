96c612191b64c3d65dbf89e642d7d051ded773ca
duechayapolgmit <duechayapolsbs@gmail.com>
2026-01-26 21:38:57 +0000

    clean: overlay info logic

--- display/lib/overlayInfo.js
+++ display/lib/overlayInfo.js
@@ -4,7 +4,6 @@ import teamLabels from '@/data/team_labels.json';
 import yaml from 'js-yaml';
 import fs from 'fs';
 import path from "path";
-import { error } from 'console';
 
 const statePath = path.join(process.cwd(), "state/overlay.json");
 const stateDefaultPath = path.join(process.cwd(), "state/defaults/overlay.json")
@@ -42,43 +41,38 @@ function save(state) {
 let data = load();
 data.config = config; 
 
-export function getOverlayData() {
-    return data
-}
+/* --------------
+    GETTERS
+----------------- */ 
+export const getOverlayData = () => data;
 
-/* Controls Game Numbers */
-export function getGameNumber() {
-    return data.gameNumber;
-}
+export const getGameNumber = () => data.gameNumber;
+export const getGame = () => data.game;
 
+export const getFirstPlace = () => data.first;
+export const getSecondPlace = () => data.second;
+
+export const getFirstDBPoints = () => data.firstDB;
+export const getSecondDBPoints = () => data.secondDB;
+
+export const getStatusDisplayOptions = () => data.statusVisible;
+export const getPlacementsDisplayOptions = () => data.placementsVisible;
+
+/* --------------
+    SETTERS
+----------------- */ 
 export function setGameNumber(gameNo) {
     data.gameNumber = gameNo
-
     // Check the multiplier associated and attach the multiplier with that (default x1.0)
-    data.multiplier = multipliers[data.gameNumber] || "x1.0";
+    data.multiplier = multipliers[data.gameNumber] || "x1.0"; 
     save(data);
-}
-
-/* Controls Game Logos */
-export function getGame() {
-    return data.game;
 }
 
 export function setGame(game) {
     data.game = game
-
     // Check the game logo associated
     data.gameLogo = gameLogos[data.game] || "/game_logos/Default.png";
     save(data);
-}
-
-/* Controls Standings */
-export function getFirstPlace() {
-    return data.first
-}
-
-export function getSecondPlace() {
-    return data.second
 }
 
 export function setFirstPlace(team) {
@@ -97,15 +91,6 @@ export function setSecondPlace(team) {
     save(data);
 }
 
-/* Controls DB score */
-export function getFirstDBPoints() {
-    return data.firstDB;
-}
-
-export function getSecondDBPoints() {
-    return data.secondDB;
-}
-
 export function setFirstDBPoints(points) {
     data.firstDB = points;
     save(data);
@@ -116,27 +101,18 @@ export function setSecondDBPoints(points) {
     save(data);
 }
 
-/* Controls animations */
-export function getStatusDisplayOptions() {
-    return data.statusVisible;
-}
-
-export function getPlacementsDisplayOptions() {
-    return data.placementsVisible;
-}
-
 export function setStatusDisplayOptions(option) {
-    if (typeof option != "boolean") return;
-
-    data.statusVisible = option;
-    save(data);
+    if (typeof option == "boolean") {
+        data.statusVisible = option;
+        save(data);
+    } 
 }
 
 export function setPlacementsDisplayOptions(option) {
-    if (typeof option != "boolean") return;
-
-    data.placementsVisible = option;
-    save(data);
+    if (typeof option == "boolean") {
+        data.placementsVisible = option;
+        save(data);
+    }   
 }
 
 /* RESET */
