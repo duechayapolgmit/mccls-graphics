0ac6f33c80e272b6cd6bbb275ad8625e8fba4e6c
duechayapolgmit <duechayapolsbs@gmail.com>
2026-03-01 00:27:37 +0000

    feat: voting config via control panel

--- display/app/page.tsx
+++ display/app/page.tsx
@@ -2,6 +2,7 @@ import styles from './main.module.css';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
+  
   const overlayData = await fetch('http://localhost:3000/api/overlay', { cache: 'no-store' })
     .then(r => r.json());
   
@@ -18,9 +19,26 @@ export default async function Home() {
     redirect('/');
   }
 
+  async function updateVoting(formData: FormData) {
+    'use server';
+
+    const params = new URLSearchParams();
+    formData.forEach((value, key) => params.append(key, value.toString()));
+
+    await fetch(`http://localhost:3000/api/voting?${params.toString()}`);
+
+   
+  }
+
   async function resetOverlay(formData: FormData) {
     'use server';
     await fetch('http://localhost:3000/api/overlay?reset=true')
+    redirect('/')
+  }
+
+  async function resetVoting(formData: FormData) {
+    'use server';
+    await fetch('http://localhost:3000/api/voting?reset=true')
     redirect('/')
   }
 
@@ -65,12 +83,14 @@ export default async function Home() {
         <span className={styles.entry_heading_extended}>Status</span>
         <form action={updateOverlay}>
           <input type="text" name="status" defaultValue={showHide(overlayData.statusVisible)}/>
+          <button type="submit">OK</button>
         </form>
       </div>
       <div className={styles.entry}>
         <span className={styles.entry_heading_extended}>Placements</span>
         <form action={updateOverlay}>
           <input type="text" name="placements" defaultValue={showHide(overlayData.placementsVisible)}/>
+          <button type="submit">OK</button>
         </form>
       </div>
 
@@ -80,7 +100,32 @@ export default async function Home() {
       </form>
 
       <hr/>
-
+      <h2>Voting</h2>
+      <h3>Voting Slots</h3>
+      {votingData.slots.map((slot: any, i: number) => (
+        <div key={i} className={styles.entry}>
+          <span className={styles.entry_heading}>{i + 1}</span>
+          <form action={updateVoting}>
+            <input type="hidden" name="slot" value={i + 1} />
+            <input type="text" name="game" defaultValue={slot.game} required />
+            <button type="submit">OK</button>
+          </form>
+        </div>
+      ))}
+      <h3>Choose a game based on slot (have to manually refresh to update everything on here)</h3>
+      <form className={styles.entry} action={updateVoting}>
+        <input name="slotChosen" type="number"/>
+        <button type="submit">OK</button>
+      </form>
+      <h3>Toggle Display (show/hide)</h3>
+      <form className={styles.entry} action={updateOverlay}>
+        <input type="text" name="status" defaultValue={showHide(votingData.visible)}/>
+        <button type="submit">OK</button>
+      </form>
+      <h3>Reset Overlay</h3>
+      <form className={styles.entry} action={resetVoting}>
+        <input type="submit" value="RESET OVERLAY (can't be undone)"/>
+      </form>
     </div>
   );
 }
