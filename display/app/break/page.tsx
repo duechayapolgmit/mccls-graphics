4cd415a2a7b5d277c38584e9eedb7ba632587d64
duechayapolgmit <duechayapolsbs@gmail.com>
2026-04-22 04:18:55 +0100

    feature: polished layout to be coded for break screens

--- display/app/break/page.tsx
+++ display/app/break/page.tsx
@@ -19,17 +19,21 @@ export default function Page() {
     return (
         <div className={styles.main}>
             <div className={styles.header}>
-                <div className={styles.header_icon} style={{"--bg-colour": config.colours.secondary} as React.CSSProperties}><img src={"/icon-event.png"}/></div>
+                <div className={styles.icon} style={{"--bg-colour": config.colours.secondary} as React.CSSProperties}><img src={"/icon-event.png"}/></div>
                 <div className={styles.header_text}>TESTTESTTESTTEST</div>
+                <div className={styles.right_text}>1:00</div>
+                <div className={`${styles.icon} ${styles.right_icon}`} style={{"--bg-colour": config.colours.secondary} as React.CSSProperties}></div>
             </div>
             <div className={styles.body}>
-                <div className={styles.body_content} style={{"--scale": getScaleSize()} as React.CSSProperties}>
-                    <CardGrid  lst={lst}/>
+                <div className={styles.content_cards} style={{"--scale": getScaleSize()} as React.CSSProperties}>
+                    <CardGrid lst={lst}/>
                 </div>
             </div>
             <div className={styles.footer}>
-                <div className={styles.header_icon} style={{"--bg-colour": config.colours.secondary} as React.CSSProperties}></div>
-                <div className={styles.header_text}>MC Championship: Project G.L.O.P.</div>                
+                <div className={styles.icon} style={{"--bg-colour": config.colours.secondary} as React.CSSProperties}></div>
+                <div className={styles.header_text}>{config.info.event_name}: <span className='text-colour' style={{"--text-colour": config.colours.highlight} as React.CSSProperties}>{config.info.tagline}</span></div>
+                <div className={styles.right_text}>MCC Live Show</div>
+                <div className={`${styles.icon} ${styles.right_icon}`} style={{"--bg-colour": config.colours.secondary} as React.CSSProperties}></div>        
             </div>
         </div>
     )
