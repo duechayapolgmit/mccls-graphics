eeb8649a486da2c43fae72032ed71d89c3a47722
duechayapolgmit <duechayapolsbs@gmail.com>
2026-04-22 20:49:10 +0100

    refactor: votingHandler to server

--- display/app/api/voting/subscribe/route.ts
+++ display/app/api/voting/subscribe/route.ts
@@ -2,7 +2,7 @@ export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
 import { newStream } from '@/lib/transmitter/helper';
-import { getData } from '@/lib/voting/votingInfo';
+import { getData } from '@/lib/server/votingHandler';
 
 export async function GET() {
   return newStream(getData(), "voting");
